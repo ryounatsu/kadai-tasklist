e80fcb8835b9b692a9d7f9120bbe56f3e109a917
natsuki.katou <ntk101628@outlook.jp>
2024-05-22 19:50:02 +0900

    Lesson163つ目の課題終了

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -30,7 +30,8 @@ public class EditServlet extends HttpServlet {
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
         // 該当のIDのメッセージ1件のみをデータベースから取得
@@ -43,8 +44,9 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
 
         // メッセージIDをセッションスコープに登録
-        request.getSession().setAttribute("task_id", m.getId());
-
+        if (m != null) {
+            request.getSession().setAttribute("task_id", m.getId());
+        }
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
     }
